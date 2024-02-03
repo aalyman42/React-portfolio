892e9743c92f30bd353a765d8b938979b9a84a23
aalyman42 <aalyman42@gmail.com>
2024-02-02 21:22:58 -0700

    added router structure to main jsx

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,5 +1,39 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import App from "./App";
+import Projects from "./pages/Projects";
+import About from "./page/About";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/Projects",
+        element: <Projects />,
+      },
+      {
+        path: "/About",
+        element: <About />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
