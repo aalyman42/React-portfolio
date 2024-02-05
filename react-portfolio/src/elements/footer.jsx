fd2377a88a18db04517746ff973028391e9e9782
aalyman42 <aalyman42@gmail.com>
2024-02-05 16:11:15 -0700

    finished homepage

--- react-portfolio/src/elements/footer.jsx
+++ react-portfolio/src/elements/footer.jsx
@@ -1,4 +1,82 @@
+import { Link, useLocation } from "react-router-dom";
+import "./styles/footer.css";
+
 function footer() {
-  return;
+  const currentPage = useLocation().pathname;
+  return (
+    <footer class="text-center text-lg-start">
+      <div class="container p-4">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase">pages</h5>
+            <ul>
+              <li>
+                <Link
+                  to="/"
+                  className={
+                    currentPage === "/" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/About"
+                  className={
+                    currentPage === "/About" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Projects"
+                  className={
+                    currentPage === "/Projects" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Contact"
+                  className={
+                    currentPage === "/Contact" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Links</h5>{" "}
+            <a href="https://github.com/aalyman42" class="mx-2" target="_blank">
+              <img src="/github.svg" alt="Github logo" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/aaron-lyman-b2171315a/"
+              target="_blank"
+            >
+              <img src="/LinkedIn.svg" alt="LinkedIn logo" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3">
+        Footer: © 2020 Copyright:
+        <a class="text-body" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
 }
 export default footer;
--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -1,48 +1,89 @@
+import { Link, useLocation } from "react-router-dom";
+import "./styles/home.css";
 export default function Home() {
+  const currentPage = useLocation().pathname;
   return (
     <div class="container text-center">
       <div class="row align-items-start">
         <div class="col-12">
           <h2>Aaron Lyman</h2>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
-            laudantium quaerat, eos accusantium eius odit deserunt aliquam
-            aliquid. Cupiditate nobis, pariatur, cum nihil nostrum excepturi
-            quae repellendus praesentium consequuntur quis maxime? Alias nihil
-            amet fugiat dicta necessitatibus laborum modi consectetur vel
-            dolores praesentium perferendis tempora molestiae, laboriosam
-            quaerat aspernatur voluptatum?
+            I'm a new and eager boot-camp graduate with a background in
+            education. A love for learning and problem solving drive my everyday
+            life! I am married to my beautiful wife Kyleigh and we live with our
+            dog Opal.{" "}
           </p>
+          <p>
+            In my bachelor program at the University of northern Colorado, I
+            majored in Journalism and minored in Earth Science. I have always
+            felt draws to both the creative and the concrete which all lends
+            itself fantastically to coding where exisiting in a system and
+            thinking out of the box are equally important.{" "}
+          </p>
+          <p>
+            {" "}
+            In my free time I love playing video games and reading Japanese
+            Manga. I love almost all things geeky from D&D to Doctor Who.
+          </p>
+
           <button type="button" class="btn btn-danger">
-            Read more
+            <Link
+              to="/About"
+              className={
+                currentPage === "/About" ? "nav-link active" : "nav-link"
+              }
+              class="nav-link"
+            >
+              My skills and work history
+            </Link>
           </button>
         </div>
+        <img src="/page-break1.png" alt="" />
         <br />
-        <div class="col my-5">
+        <div class="col-sm-12 col-md-6 my-2">
           <div class="card">
             <div class="card-body">
               <h3>Projects</h3>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
-                delectus? Non, ullam ex nesciunt adipisci fugit eligendi ipsam
-                voluptatum debitis voluptatem unde dolorum id.
+                Throughout my time at coding boot-camp, I worked on some big
+                projects. They each show off various coding architectures and
+                skillsets. Across all of them are an overarching practice at
+                coding as a team and managing a gitflow.
               </p>
               <button type="button" class="btn btn-danger">
-                see the projects
+                <Link
+                  to="/Projects"
+                  className={
+                    currentPage === "/Projects" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  See my projects
+                </Link>
               </button>
             </div>
           </div>
         </div>
-        <div class="col my-5">
+        <div class="col my-2">
           <div class="card">
             <div class="card-body">
               <h3>Contact</h3>
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
-                delectus?
+                I am currently looking for a great place to work at that can put
+                my skills to practice. If I have caught your interest, please
+                reach out! I would love to be in contact and talk about any
+                projects you are working on.
               </p>
               <button type="button" class="btn btn-danger">
-                Contact me
+                <Link
+                  to="/Contact"
+                  className={
+                    currentPage === "/Contact" ? "nav-link active" : "nav-link"
+                  }
+                  class="nav-link"
+                >
+                  Contact me
+                </Link>
               </button>
             </div>
           </div>
