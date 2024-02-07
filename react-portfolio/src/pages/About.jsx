6f975b87fc7da947873618fb29aa0b25da44338b
aalyman42 <aalyman42@gmail.com>
2024-02-07 08:49:16 -0700

    finished about me page

--- react-portfolio/src/pages/About.jsx
+++ react-portfolio/src/pages/About.jsx
@@ -1,19 +1,45 @@
-export default function Home() {
+import("./styles/main.css");
+export default function About() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div class="container">
+      <div class="row align-items-start">
+        <div class="col-sm-12 col-md-6">
+          <img src="/images/IMG_7047.jpg" alt="Aaron" class="my-3" />
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <h2 class="my-2">About Me</h2>
+          <p>
+            I grew up in Greeley, Colorado with my 3 older brothers. As I grew
+            up, I took interest in many different things like reading and video
+            games. Throughout my life, I had always ahd interests in creative
+            writing and stroy-telling, but also technical things like computer
+            software and Hardware. at 18 I built my first PC and hve been doing
+            it as a hobby since. My undergraduate degree was a BA in Journalism
+            with a minor in Earth Science. I graduated directly into the
+            pandameic and was at a total loss for jobs. At this time I continued
+            to work for my internship site, The Johnstown Breeze as I picked up
+            substitute teaching. After a few years of being a substitute, I
+            spent a year teaching English at the charter school Leman Academy of
+            Excellence.
+          </p>
+          <p>
+            In my time as a teacher I took a lot of time to reflect on my
+            professional life. I decided that work was most satisfying for me in
+            a role that focused on technical performance and learning. Because
+            of my love for tech, I enrolled in the full stack coding bootcamp at
+            DU that I finished off in February. So far coding has given me such
+            a great experience I havent' gotten from any other job and every day
+            feels exciting and rewarding. I am excited to enter into the field
+            and I feel like I have truly found a job I can do for life.
+          </p>
+          <p>
+            Right now I live in Lone Tree with my beautiful wife Kyleigh and out
+            dog Opal. Kyleigh works as an audiologist at Integrated Ear, Nose,
+            and Throat. We both hope to find steady jobs so that we may grow our
+            family in the future.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
